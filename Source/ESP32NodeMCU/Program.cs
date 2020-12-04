9c7751bb675584edf918279eb0ef3170bc001699
RolandoMagico <75312877+RolandoMagico@users.noreply.github.com>
2020-12-04 16:17:18 +0100

    Simple Wifi network scan

--- Source/ESP32NodeMCU/Program.cs
+++ Source/ESP32NodeMCU/Program.cs
@@ -1,20 +1,39 @@
-using System;
-using System.Diagnostics;
-using System.Threading;
-
 namespace ESP32NodeMCU
 {
+  using System.Diagnostics;
+  using System.Threading;
+  using Windows.Devices.WiFi;
+
   public class Program
   {
     public static void Main()
     {
-      Debug.WriteLine("Hello from nanoFramework!");
+      WiFiAdapter[] adapters = WiFiAdapter.FindAllAdapters();
+      if (adapters.Length == 1)
+      {
+        WiFiAdapter adapter = adapters[0];
+        adapter.AvailableNetworksChanged += Adapter_AvailableNetworksChanged;
+        adapter.ScanAsync(); 
+      }
 
       Thread.Sleep(Timeout.Infinite);
 
       // Browse our samples repository: https://github.com/nanoframework/samples
       // Check our documentation online: https://docs.nanoframework.net/
       // Join our lively Discord community: https://discord.gg/gCyBu8T
+    }
+
+    private static void Adapter_AvailableNetworksChanged(WiFiAdapter sender, object e)
+    {
+      Debug.WriteLine(string.Empty);
+      WiFiAvailableNetwork[] networks = sender.NetworkReport.AvailableNetworks;
+      Debug.WriteLine($"Network scan complete, found {networks.Length} networks");
+      foreach (var network in networks)
+      {
+        Debug.WriteLine(network.Ssid);
+      }
+
+      sender.ScanAsync();
     }
   }
 }
